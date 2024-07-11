2c0d705f126c43c67f78459aac308119238d4371
Faza Zikri <fazadoang15@gmail.com>
2024-07-11 16:31:19 +0700

    feat(docker): dockerize the app

---
+++ apps/api/src/controllers/user.controller.ts
@@ -0,0 +1,46 @@
+import { Request, Response } from "express";
+import prisma from "@/prisma";
+
+export class UserController {
+	async getUsers(req: Request, res: Response) {
+		try {
+			const users = await prisma.user.findMany();
+			return res.status(200).send(users);
+		} catch (error) {
+			return res.status(500).send({ error: "Failed to fetch users" });
+		}
+	}
+
+	async getUserById(req: Request, res: Response) {
+		try {
+			const { user_id } = req.params;
+
+			const user = await prisma.user.findUnique({
+				where: { user_id: Number(user_id) },
+			});
+
+			if (!user) {
+				return res.status(404).send({ error: "User not found" });
+			}
+
+			return res.status(200).send(user);
+		} catch (error) {
+			return res.status(500).send({ error: "Failed to fetch user" });
+		}
+	}
+
+	async createUser(req: Request, res: Response) {
+		try {
+			const { username, password, email, full_name, phone_number, role } =
+				req.body;
+
+			const newUser = await prisma.user.create({
+				data: { username, password, email, full_name, phone_number, role },
+			});
+
+			return res.status(201).send(newUser);
+		} catch (error) {
+			return res.status(500).send({ error: "Failed to create user" });
+		}
+	}
+}
