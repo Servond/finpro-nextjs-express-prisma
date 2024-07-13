64f864e2bc10ad284119ca6cbcd38bec20760e35
Faza Zikri <fazadoang15@gmail.com>
2024-07-13 22:31:56 +0700

    fix(db): fix api not getting output from prisma

--- apps/api/src/prisma.ts
+++ apps/api/src/prisma.ts
@@ -1,3 +1,20 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
-export default new PrismaClient({ log: ['query', 'info', 'warn', 'error'] });
+declare global {
+	var prisma: PrismaClient | undefined;
+}
+
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === "production") {
+	prisma = new PrismaClient({ log: ["query", "info", "warn", "error"] });
+} else {
+	if (!global.prisma) {
+		global.prisma = new PrismaClient({
+			log: ["query", "info", "warn", "error"],
+		});
+	}
+	prisma = global.prisma;
+}
+
+export default prisma;
