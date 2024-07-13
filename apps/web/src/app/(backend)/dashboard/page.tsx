51031d708a8724a5d4c52fbf9502b71e1647b21e
Faza Zikri <fazadoang15@gmail.com>
2024-07-13 23:54:55 +0700

    feat(fetch): add tanstack query for fetching and other packages
    add tanstack, react-hook-form, and zod

--- apps/web/src/app/(backend)/dashboard/page.tsx
+++ apps/web/src/app/(backend)/dashboard/page.tsx
@@ -1,7 +1,24 @@
+'use client';
+
+import { useQuery } from '@tanstack/react-query';
+
 const Page = () => {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['todos'],
+    queryFn: () =>
+      fetch('https://jsonplaceholder.typicode.com/todos').then((res) =>
+        res.json(),
+      ),
+  });
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+
   return (
     <div>
-      <h1>Hello, world</h1>
+      {data.map((todo: any) => (
+        <div key={todo.id}>{todo.id}</div>
+      ))}
     </div>
   );
 };
