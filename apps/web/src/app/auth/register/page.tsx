9cbd9ca8d8a39513aff07bb032de2504de227c16
Faza Zikri <fazadoang15@gmail.com>
2024-07-21 01:40:07 +0700

    feat(auth): add login and register page

--- apps/web/src/app/auth/register/page.tsx
+++ apps/web/src/app/auth/register/page.tsx
@@ -1,9 +1,21 @@
 import RegisterForm from '@/components/auth/RegisterForm';
+import Image from 'next/image';
 
 const RegisterPage = async () => {
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <RegisterForm />
+    <div className="grid md:grid-cols-2 w-full min-h-screen">
+      <div className="hidden md:block relative">
+        <Image
+          src="/login_bg.jpg"
+          alt="Register Image"
+          width={1920}
+          height={1080}
+          className="absolute inset-0 h-full w-full object-cover"
+        />
+      </div>
+      <div className="flex items-center justify-center px-4 py-12 md:px-6">
+        <RegisterForm />
+      </div>
     </div>
   );
 };
