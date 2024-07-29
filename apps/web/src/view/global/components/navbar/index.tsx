78145ae46a7ac119d3576c5bacfeac437237518c
Faza Zikri <fazadoang15@gmail.com>
2024-07-29 14:23:04 +0700

    fix: navbar

--- apps/web/src/view/global/components/navbar/index.tsx
+++ apps/web/src/view/global/components/navbar/index.tsx
@@ -39,39 +39,6 @@ const Navbar = async () => {
           </Box>
           {!user ? (
             <Stack direction="row" spacing={1}>
-<<<<<<< HEAD
-              <Button variant="outlined" onClick={() => router.push("/login")}
-                sx={{
-                  border: 'none',
-                  color: 'black',
-                  '&:hover': {
-                    backgroundColor: "white",
-                    border: 'none',
-                    color: 'rgb(238 46 36)'
-                  }
-                }}
-                >
-                Login
-              </Button>
-              <Button
-                variant="outlined"
-                onClick={() => router.push("/register")}
-                sx={{
-                  border: 'none',
-                  color: 'black',
-                  borderRadius: '999px',
-                  transition: 'background-color 0.3s, color 0.3s',
-                  '&:hover': {
-                    backgroundColor: "rgb(238 46 36)",
-                    color: 'white',
-                    border: 'none', 
-                    borderRadius: '999px'
-                  }
-                }}
-                >
-                Register
-              </Button>
-=======
               <form
                 action={async () => {
                   'use server';
@@ -117,7 +84,6 @@ const Navbar = async () => {
                   Register
                 </Button>
               </form>
->>>>>>> main
             </Stack>
           ) : (
             <div className="flex gap-4">
